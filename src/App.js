c0a08571a4d79d7527b7a8929b4a647ccc126d3c
sedx876 <swatkins876@gmail.com>
2020-10-12 08:20:08 -0400

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import SearchFilter from './SearchFilter'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+    <SearchFilter />
+    )
+  }
 }
 
 export default App;
